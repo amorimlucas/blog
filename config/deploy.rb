0213449e4092812a298c7f0ab1161a4df0b73764
Lucas Amorim <amorimluc@gmail.com>
2019-08-11 01:45:22 -0300

    Config changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.11.0"
 set :application, "blog"
 set :repo_url, "git@github.com:amorimlucas/blog.git"
 set :deploy_to, "/var/www/blog"
-append :linked_files, "config/database.yml"
-#append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_files, "config/database.yml", "config/master.key"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 set :keep_releases, 5
 set :ssh_options, forward_agent: true
 set :rbenv_ruby, '2.6.3'
---
+++ config/unicorn.rb
@@ -0,0 +1,22 @@
+app_path = "/var/www/blog/current"
+worker_processes 1
+listen app_path + '/tmp/unicorn.sock', backlog: 1024
+timeout 300
+working_directory app_path
+pid app_path + '/tmp/unicorn.pid'
+stderr_path app_path + '/log/unicorn.log'
+stdout_path app_path + '/log/unicorn.log'
+preload_app true
+
+GC.respond_to?(:copy_on_write_friendly=) &&
+  GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
