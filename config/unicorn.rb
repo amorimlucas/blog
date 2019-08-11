12e365c6ef7d54e3cc9ee3eec2962e12f8ff79a9
Lucas Amorim <amorimluc@gmail.com>
2019-08-11 02:06:18 -0300

    Config changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,5 @@
-app_path = "/var/www/blog/current"
+app_path = File.expand_path(File.dirname(__FILE__) + '/..')
+#app_path = "/var/www/blog/current"
 worker_processes 1
 listen app_path + '/tmp/unicorn.sock', backlog: 1024
 timeout 300
